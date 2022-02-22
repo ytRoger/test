fb82ddb8612771db2ac29cecefd57bf59309854e
CRmis-git <49969016+crmis-git@users.noreply.github.com>
2022-02-22 14:23:00 +0800

    Update test.php

--- test.php
+++ test.php
@@ -3,6 +3,5 @@
     echo 'TEST2<br>';
     $bear = false;
     $panda = true;
-    $firefox = $panda && $bear;
-    $firefox = $panda && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear && $bear;
+    $koala  = $bear && $panda;
 ?>
